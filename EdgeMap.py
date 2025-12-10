dfe6d9088b964aa617b7deb328712fd0623f64cb
Diisshhhaa <156817760+disha-katkade@users.noreply.github.com>
2025-12-10 21:27:57 +0530

    Delete EdgeMap.py

--- EdgeMap.py
+++
@@ -1,7 +0,0 @@
-import cv2
-img = cv2.imread('D:/4th year/10th SEM - INTERIM SEM 2025-26/CSE3010/Practicals/cube.jpg')
-laplacian = cv2.Laplacian(img,cv2.CV_64F)
-#shows the image
-cv2.imshow('EdgeMap image',laplacian)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
