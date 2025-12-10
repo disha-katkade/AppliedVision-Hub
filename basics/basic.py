ab42cbdb34fb77503f54ccae907d92f807bc5016
Diisshhhaa <156817760+disha-katkade@users.noreply.github.com>
2025-12-10 21:45:17 +0530

    Add files via upload
    reading, writing, and displaying an image

---
+++ basics/basic.py
@@ -0,0 +1,14 @@
+#Read an image, store, change the color, show it, write in a file
+import cv2
+path = 'D:/4th year/10th SEM - INTERIM SEM 2025-26/CSE3010/Practicals/cube.jpg'
+img2 = cv2.imread(path)
+#img = cv2.imread(path,0) --- gray
+#img1 = cv2.imread(path,-1) --- unchanged
+#img2 = cv2.imread(path,1)
+while(True):
+    cv2.imshow('color image',img2)
+    a = input('Do you break? y/n')
+    if a== 'y':
+        break
+file = input('Enter the image name to be saved')
+cv2.imwrite(file+'.jpg',img2)
