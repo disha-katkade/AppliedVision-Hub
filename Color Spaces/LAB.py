28aa49ccba066887ecc600f05592b50340a0c51f
Diisshhhaa <156817760+disha-katkade@users.noreply.github.com>
2025-12-10 22:02:54 +0530

    RGB to LAB image

---
+++ Color Spaces/LAB.py
@@ -0,0 +1,12 @@
+#Import cv2 module
+import cv2
+#Read the image
+img = cv2.imread('D:/4th year/10th SEM - INTERIM SEM 2025-26/CSE3010/Practicals/cube.jpg')
+
+#convert to YCrCb color space
+img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+
+#shows the image
+cv2.imshow('LAB image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
