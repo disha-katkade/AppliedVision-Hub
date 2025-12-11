b4d6d742c26f961b5bf36133032d77de2ce35902
Diisshhhaa <156817760+disha-katkade@users.noreply.github.com>
2025-12-11 12:47:16 +0530

    Update Log_Transformation.py

--- Image-Transformation/Log_Transformation.py
+++ Image-Transformation/Log_Transformation.py
@@ -2,7 +2,7 @@
 import cv2
 import  matplotlib.pyplot as plt
 import numpy as np
-img = cv2.imread('D:/4th year/10th SEM - INTERIM SEM 2025-26/CSE3010/Practicals/cube.jpg',0)
+img = cv2.imread('input image.jpg',0)
 log = 0.6 *(np.log(1+np.float32(img)))
 
 fig = plt.figure(figsize=(15,5))
@@ -13,4 +13,5 @@ ax = fig.add_subplot(122)
 plt.imshow(log,cmap='gray')
 ax.set_title('Log Transform',fontsize = 18)
 plt.show()
+
 
