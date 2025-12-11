29a9c3af118918d48fe126e80f2998d4fc4e98c7
Diisshhhaa <156817760+disha-katkade@users.noreply.github.com>
2025-12-11 12:46:43 +0530

    Update Inverse_Transformation.py

--- Image-Transformation/Inverse_Transformation.py
+++ Image-Transformation/Inverse_Transformation.py
@@ -2,7 +2,7 @@
 import cv2
 import  matplotlib.pyplot as plt
 import numpy as np
-img = cv2.imread('D:/4th year/10th SEM - INTERIM SEM 2025-26/CSE3010/Practicals/cube.jpg',0)
+img = cv2.imread('input-image.jpg',0)
 img_inverse = 255 - img
 
 fig = plt.figure(figsize=(15,5))
@@ -12,4 +12,5 @@ plt.imshow(img,cmap = 'gray')
 ax = fig.add_subplot(122)
 plt.imshow(img_inverse,cmap='gray')
 ax.set_title('Inverse Transform',fontsize = 18)
+
 plt.show()
