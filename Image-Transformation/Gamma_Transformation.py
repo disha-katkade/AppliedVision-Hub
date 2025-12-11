7bf5c83ce2172ed999a19b8be0ef93f2903b0041
Diisshhhaa <156817760+disha-katkade@users.noreply.github.com>
2025-12-11 12:46:16 +0530

    Update Gamma_Transformation.py

--- Image-Transformation/Gamma_Transformation.py
+++ Image-Transformation/Gamma_Transformation.py
@@ -2,7 +2,7 @@
 import cv2
 import  matplotlib.pyplot as plt
 import numpy as np
-img = cv2.imread('D:/4th year/10th SEM - INTERIM SEM 2025-26/CSE3010/Practicals/cube.jpg',0)
+img = cv2.imread('input image.jpg',0)
 img1 = img/255.0
 img_power_law_transformation = cv2.pow(img1,1.8) #to change the size of the figure
 
@@ -13,4 +13,5 @@ plt.imshow(img,cmap = 'gray')
 ax = fig.add_subplot(122)
 plt.imshow(img_power_law_transformation,cmap='gray')
 ax.set_title('Power Law Transform',fontsize = 18)
+
 plt.show()
