b29af5007e57ce62ebee452b55e752afb3d35ad3
Diisshhhaa <156817760+disha-katkade@users.noreply.github.com>
2025-12-11 12:41:36 +0530

    data visualization files

---
+++ Feature- Visualization/SpectralMap.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+import cv2
+img = cv2.imread('D:/4th year/10th SEM - INTERIM SEM 2025-26/CSE3010/Practicals/cube.jpg')
+plt.imshow(img,cmap = 'nipy_spectral')
+plt.show()
