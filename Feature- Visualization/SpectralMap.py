7ab2192acb48858eff9333aff609f10c7eb383dc
Diisshhhaa <156817760+disha-katkade@users.noreply.github.com>
2025-12-12 11:55:38 +0530

    Update SpectralMap.py

--- Feature- Visualization/SpectralMap.py
+++ Feature- Visualization/SpectralMap.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import cv2
-img = cv2.imread('D:/4th year/10th SEM - INTERIM SEM 2025-26/CSE3010/Practicals/cube.jpg')
+img = cv2.imread('input image.jpg')  #write input image path here
 plt.imshow(img,cmap = 'nipy_spectral')
+
 plt.show()
