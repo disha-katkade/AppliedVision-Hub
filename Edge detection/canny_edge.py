757510cbd89774c7f1e70a86aa25364d8ae57d84
Diisshhhaa <156817760+disha-katkade@users.noreply.github.com>
2025-12-12 11:49:45 +0530

    Update canny_edge.py

--- Edge detection/canny_edge.py
+++ Edge detection/canny_edge.py
@@ -3,7 +3,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 # image reading and preproceesing
-img = cv2.imread("D:/4th year/10th SEM - INTERIM SEM 2025-26/CSE3010/Practicals2/cube_img.jpg")
+img = cv2.imread("input image path.jpg")
 imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 img_gray = cv2.cvtColor(imgRGB, cv2.COLOR_RGB2GRAY)
 img_blur = cv2.GaussianBlur(img_gray,(3,3),0)
@@ -16,4 +16,5 @@ edges = cv2.cvtColor(edges,cv2.COLOR_BGR2RGB)
 cv2.imshow("Edges",edges)
 
 cv2.waitKey(0)
+
 cv2.destroyAllWindows()
