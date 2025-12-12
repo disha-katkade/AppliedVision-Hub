ddf6f7a780d1232e89f910d6b604ba4a6f071f98
Diisshhhaa <156817760+disha-katkade@users.noreply.github.com>
2025-12-12 11:50:27 +0530

    Update EdgeMap.py

--- Edge detection/EdgeMap.py
+++ Edge detection/EdgeMap.py
@@ -1,8 +1,9 @@
 import cv2
-img = cv2.imread('input image.jpg')
+img = cv2.imread('input image.jpg')   #input image path
 laplacian = cv2.Laplacian(img,cv2.CV_64F)
 #shows the image
 cv2.imshow('EdgeMap image',laplacian)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
+
