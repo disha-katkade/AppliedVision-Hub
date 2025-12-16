78c643dc57507fea8cc8abaf9fbf3cab25f88c95
Diisshhhaa <156817760+disha-katkade@users.noreply.github.com>
2025-12-16 23:23:03 +0530

    Update Morphological_operations.py

--- Image-Transformation/Morphological_operations.py
+++ Image-Transformation/Morphological_operations.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 # reading the image
-img = cv2.imread('D:/4th year/10th SEM - INTERIM SEM 2025-26/CSE3010/Practicals/cube.jpg')
+img = cv2.imread('input image.jpg')   #put input image here
 # setting up the kernel
 kernel = np.ones((5,5),np.uint8)
 plt.imshow(img)
@@ -18,4 +18,5 @@ plt.show()
 # dilation
 dilation = cv2.dilate(img,kernel)
 plt.imshow(dilation)
+
 plt.show()
