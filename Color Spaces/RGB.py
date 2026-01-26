a57d1b3e696c5d4113b86222aadec38d3bf56852
Diisshhhaa <156817760+disha-katkade@users.noreply.github.com>
2026-01-26 23:33:56 +0530

    Update RGB.py

--- Color Spaces/RGB.py
+++ Color Spaces/RGB.py
@@ -1,7 +1,8 @@
 #python program to read imgae as RGB
 import cv2
 import matplotlib.pyplot as plt
-img = cv2.imread('input image.jpg')   #input image path
+img = cv2.imread('input image.jpg')   #paste input image path
 plt.imshow(img)
 
 plt.show()
+
